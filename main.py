0089a6c31e8eec2bcbcce3372a8e2eecf9d66861
mushirih <pmwachira@gmail.com>
2025-07-16 10:44:49 +0300

    added logging

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import json
 import pandas as pd
+from pathlib import Path
 
 
-data = r'input_data/2023 DE_case_dataset.gz.parquet'
+data_file = r'input_data/2023 DE_case_dataset.gz.parquet'
+log_data = r'log/uuid_history.txt'
 # input_data reduction
 columns = ['uuid', 'holes']
 # ingest input_data
@@ -11,8 +13,7 @@ columns = ['uuid', 'holes']
 # display output
 # filter to show only relevant orders
 
-def ingest_data(data):
-    df = pd.read_parquet(data, columns=columns)
+def ingest_data(df):
     count_rows_processed = 0
     count_has_unreachable_hole_warning = 0
     count_has_unreachable_hole_error = 0
@@ -51,11 +52,35 @@ def ingest_data(data):
 
         count_rows_processed+=1
 
+        with open(log_data, 'a') as f:
+            f.write(str(item.uuid) + "\n")
+
     return df, count_rows_processed, count_has_unreachable_hole_warning, count_has_unreachable_hole_error
 
+
+def check_history():
+    with open(log_data, "r") as f:
+        uuid_list = [line.strip() for line in f if line.strip()]
+
+    df = pd.DataFrame(uuid_list, columns=["uuid"])
+
+    return df
+
+
+def fetch_unprocessed(processed):
+    df = pd.read_parquet(data_file, columns=columns)
+    data = df[~df['uuid'].isin(processed['uuid'])]
+    return data
+
+
 if __name__ == '__main__':
-    enriched_df, count_rows_processed, count_has_unreachable_hole_warning, count_has_unreachable_hole_error = ingest_data(data)
-    data_df = pd.read_parquet(data)
-    df_merge = pd.merge(data_df, enriched_df, on='uuid')
-    print('Rows processed: '+str(count_rows_processed)+', Warnings: '+str(count_has_unreachable_hole_warning)+' Errors: '+str(count_has_unreachable_hole_error))
-    df_merge.to_parquet(r'output/processed_parts.parquet', index=False)
+    processed = check_history()
+    data = fetch_unprocessed(processed)
+    if len(data)>0:
+        enriched_df, count_rows_processed, count_has_unreachable_hole_warning, count_has_unreachable_hole_error = ingest_data(data)
+        data_df = pd.read_parquet(data_file)
+        df_merge = pd.merge(data_df, enriched_df, on='uuid')
+        print('Rows processed: '+str(count_rows_processed)+', Warnings: '+str(count_has_unreachable_hole_warning)+' Errors: '+str(count_has_unreachable_hole_error))
+        df_merge.to_parquet(r'output/processed_parts.parquet', index=False)
+    else:
+        print('No new data to process')
